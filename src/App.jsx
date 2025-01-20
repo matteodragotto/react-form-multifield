81af3950620754950c8123a3b1ca94cbc719f056
matteodragotto <matteodrag8@gmail.com>
2025-01-20 17:07:38 +0100

    [fix] from array state to object state

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,38 @@
 import { useState } from "react"
 
-const postTitlesList = [
-  { id: 1, title: "Exploring the Wonders of the Grand Canyon" },
-  { id: 2, title: "A Journey Through the Streets of Tokyo" },
-  { id: 3, title: "Discovering the Hidden Gems of Paris" },
-  { id: 4, title: "Adventures in the Australian Outback" },
-  { id: 5, title: "A Foodie's Guide to Italy" },
-  { id: 6, title: "The Best Beaches in the Caribbean" },
-  { id: 7, title: "Hiking the Trails of the Swiss Alps" },
-  { id: 8, title: "Cultural Experiences in India" },
-  { id: 9, title: "Road Trip Across the United States" },
-  { id: 10, title: "A Safari Adventure in South Africa" }
-];
+const defaultFormData = {
+  id: 1,
+  title: '',
+  image: '',
+  content: '',
+  category: '',
+  tags: '',
+  published: false
+}
 
 const App = () => {
 
-  const [postTitle, setPostTitle] = useState(postTitlesList)
-  const [newTitle, setNewTitle] = useState('')
+  const [formData, setFormData] = useState(defaultFormData)
 
   const submitHandler = (e) => {
     e.preventDefault()
-    setPostTitle([newTitle, ...postTitle])
+    console.log(formData);
+
+
   }
 
-  const newTitleHandler = (e) => {
-    const newTitle = {
-      id: postTitle.length + 1,
-      title: e.target.value
-    }
-    setNewTitle(newTitle)
+  const changeHandler = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
   }
 
   const removeHandler = (id) => {
     const newPostList = postTitle.filter(title => title.id !== id)
     setPostTitle(newPostList)
   }
+
   return (
     <div className="container my-5">
       <div className="row">
@@ -45,21 +43,13 @@ const App = () => {
             <input
               type="text"
               className="form-control"
+              name="title"
               placeholder="Titolo"
-              value={newTitle.title}
-              onChange={newTitleHandler}
+              value={formData.title}
+              onChange={changeHandler}
             />
             <button className="btn btn-success my-3" type="submit"> Invia</button>
           </form>
-
-          <ul className="list-group">
-            {postTitle.map(item => (
-              <li key={item.id} className="list-group-item d-flex justify-content-between">
-                {item.title}
-                <i className="fa-solid fa-square-minus remove" onClick={() => removeHandler(item.id)}></i>
-              </li>
-            ))}
-          </ul>
         </div>
       </div>
     </div>
